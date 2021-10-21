f778c7a07b5e642cc40b09b6514d0ea9891a3275
NewEXE <v.voloshyn@codetiburon.com>
2021-10-21 03:45:47 +0300

    Fix HTML wrong encoding detection: Add meta charset="utf-8" to all pages

--- src/pages/select-schedule-file.php
+++ src/pages/select-schedule-file.php
@@ -15,6 +15,7 @@ $links = Helpers::getScheduleFilesLinks($linksGettingCurlError);
 <!doctype html>
 <html lang="ru">
 <head>
+    <meta charset="utf-8">
     <title>Просмотр расписания</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
